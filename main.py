0a23502601ee58c223a9f98f2c871401ba16c60b
Guilherme Akira <akira.enoki@gmail.com>
2024-04-02 06:14:59 -0300

    Adicionando parte 3

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from loguru import logger
 
-logger.add("meu_app.log")
+logger.add("meu_log.log", level="CRITICAL")
 
 def soma(x,y):
-    logger.info(x)
-    logger.info(y)
-    logger.info(x + y)
-    return x + y
+   try:
+        soma = x + y
+        logger.info(f"Você digitou valores corretos, parabens {soma}")
+        return soma
+   except:
+       logger.critical("Você tem que digitar tipos corretos")
 
 
-
-print(soma(2,"3"))
+soma(2,7)
+soma(2,"3")
